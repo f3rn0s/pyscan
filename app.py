bb97c97b8aca12f37d768e676e963f7384b264c2
tealkine <tealkine2@gmail.com>
2017-03-07 11:36:47 +1000

    Added PortGuessing

--- app.py
+++ app.py
@@ -3,63 +3,100 @@ import socket
 import subprocess
 import select
 import sys
-from datetime import datetime
+from datetime import datetime as timeGet
 
 
 """
 
 TODO:
-    Implement a classified or functionified model
     Identify Services
     Pipe into easy to read output
 
 """
 
+serviceGuess = {
+                7  : 'Echo',
+                9  : 'Wake-On-Lan',
+                17 : 'Quote of the Day',
+                18 : 'Message of the Day',
+                20 : 'FTP',
+                21 : 'FTP',
+                22 : 'SSH',
+                23 : 'Telnet',
+                25 : 'SMTP',
+                53 : 'DNS',
+                69 : 'TFTP',
+                79 : 'Finger',
+                80 : 'HTTP',
+                111: 'RPC bind',
+                115: 'SFTP',
+                161: 'SNMP',
+                170: 'Print Server',
+                194: 'IRC',
+                443: 'HTTPS',
+                465: 'SMTPS',
+                531: 'AOL',
+                631: 'Cups',
+                666: 'Doom!!!',
+                843: 'Dropbox',
+                3306: 'mySQL',
+                4243: 'Docker',
+                5432:'PostGresql',
+                5433:'PostGresql'
+                }
 
-subprocess.call('clear', shell=True)
+def scanner(host, p1, p2):
+    openPorts = {}
+    t1 = timeGet.now()
+    try:
+        for port in range(p1, p2):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((host, port))
+            if result == 0:
+                # print("Port {}:  Open".format(port))
+                ready = select.select([sock], [], [], 2)
+                if ready[0]:
+                    openPorts[port] = sock.recv(1024).decode('utf-8').rstrip()
+                elif port in serviceGuess:
+                    openPorts[port] = serviceGuess[port] + "?"
+                else:
+                    openPorts[port] = "Unknown"
+            sock.close()
+        t2 = timeGet.now()
+        return openPorts, t2 - t1
+    except KeyboardInterrupt:
+        sys.exit("ByeBye\n")
+    except socket.gaierror:
+        sys.exit("Hostname could not be resolved")
+    except socket.error:
+        sys.exit("Couldn't connect\n")
 
-remoteServer = '127.0.0.1'
-# remoteServer = input("Enter a remote Host to scan: ")
-remoteServerIP = socket.gethostbyname(remoteServer)
+def scan(host='127.0.0.1', p1=0, p2=10001):
+    retList, retTime = scanner(host, p1, p2)
+    sortList = sorted(retList.items(), key=lambda x: x[0])
+    return sortList, retTime
 
-t1 = datetime.now()
+def clear():
+    subprocess.call('clear', shell=True)
 
-p1 = 0
-p2 = 65535
+def getIP(hostname):
+    return socket.gethostbyname(hostname)
 
-openPorts = {}
+clear()
 
-try:
-    for port in range(p1, p2):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print("Port {}:  Open".format(port))
-            ready = select.select([sock], [], [], 2)
-            if ready[0]:
-                openPorts[port] = sock.recv(1024).decode('utf-8').rstrip()
-            else:
-                openPorts[port] = 'unknown'
-        sock.close()
+# scanIP = getIP(input("Enter a remote Host to scan: "))
 
-except KeyboardInterrupt:
-    sys.exit("ByeBye\n")
-except socket.gaierror:
-    sys.exit("Hostname could not be resolved")
-except socket.error:
-    sys.exit("Couldn't connect\n")
+results, totalTime = scan()
 
-t2 = datetime.now()
-
-total = t2 - t1
-
-print("Initial scan took: " + str(total))
-
-sortedports = sorted(openPorts.items(), key=lambda x: x[0])
 
 print("\nResults:\n")
 
-for result in sortedports:
-    print("Port {}: ".format(result[0]) + result[1])
+print("Initial scan took: " + str(totalTime) + "\n")
+
+for result in results:
+    print("Port {}: ".format(
+                            result[0])
+             + " " * ( 5 - len(str(result[0]))) +
+                            result[1])
 
 # print(sortedports[1][0])
